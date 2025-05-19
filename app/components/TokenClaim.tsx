ed193e03bc043335b86e301e5cb87f3bd681af02
codezilar <zoeehimen@gmail.com>
2025-05-18 19:06:22 -0700

    fixed

---
+++ app/components/TokenClaim.tsx
@@ -0,0 +1,63 @@
+"use client"
+import { tokenclaim } from '../data'
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+
+const TokenClaim = () => {
+    const [activeFilter, setActiveFilter] = useState('Ongoing');
+  const filteredTokens = tokenclaim.filter(token => 
+    activeFilter === 'All' ? true : token.condition === activeFilter
+  );
+  return (
+    <div className="tokenclaims-container">
+        <div className="token-navigate">
+        <button 
+          className={activeFilter === 'Ongoing' ? 'tok-active' : ''}
+          onClick={() => setActiveFilter('Ongoing')}
+        >
+          Ongoing
+        </button>
+        <button 
+          className={activeFilter === 'Upcoming' ? 'tok-active' : ''}
+          onClick={() => setActiveFilter('Upcoming')}
+        >
+          Upcoming
+        </button>
+        <button 
+          className={activeFilter === 'Ended' ? 'tok-active' : ''}
+          onClick={() => setActiveFilter('Ended')}
+        >
+          Ended
+        </button>
+        <button 
+          className={activeFilter === 'All' ? 'tok-active' : ''}
+          onClick={() => setActiveFilter('All')}
+        >
+          All
+        </button>
+        </div>
+        <div className="tokenclaims-wrapp">
+          {filteredTokens.map(((token, index) =>(  
+            <div className="tokenclaims-content glass-card" key={index}>
+              <Image src={'/NAVIGATE_4K_S20_loop-poster@sm.webp'} alt={"nf"} width={500} height={300} className='token-img'/>
+              <div className="tokenclaims-content-text">
+                <div className="token-text-top">
+                  <p className={`current_condition ${activeFilter== "Ongoing"? "Ongoing":""} ${activeFilter== "Upcoming"? "Upcoming":""} ${activeFilter== "Ended"? "Ended":""} ${activeFilter== "All"? "All":""} `}>{token.condition}</p>
+                  <p className='date'>
+                    {token.startDate} - {token.endDate}
+                  </p>
+                </div>
+                <h2>{token.name}</h2>
+                <p>{token.desc}</p>
+                <button>{token.action}</button>
+              </div>
+            </div>
+          )))}
+        </div>
+      </div>
+
+  )
+}
+
+export default TokenClaim
--- app/tokenclaims/page.tsx
+++ app/tokenclaims/page.tsx
@@ -1,66 +1,16 @@
-"use client"
-import React, { useState } from 'react'
-import { tokenclaim } from '../data'
-import Image from 'next/image';
+import React from 'react'
+import TokenClaim from '../components/TokenClaim'
+
 
 const page = () => {
-  const [activeFilter, setActiveFilter] = useState('Ongoing');
-  const filteredTokens = tokenclaim.filter(token => 
-    activeFilter === 'All' ? true : token.condition === activeFilter
-  );
+  
   return (
     <div className='tokenclaims'>
       <h1 className='gradient-text'>Token Claims</h1>
       <div className="token-search">
         <input type="text" placeholder='Search token claims...' />
       </div>
-      <div className="tokenclaims-container">
-        <div className="token-navigate">
-        <button 
-          className={activeFilter === 'Ongoing' ? 'tok-active' : ''}
-          onClick={() => setActiveFilter('Ongoing')}
-        >
-          Ongoing
-        </button>
-        <button 
-          className={activeFilter === 'Upcoming' ? 'tok-active' : ''}
-          onClick={() => setActiveFilter('Upcoming')}
-        >
-          Upcoming
-        </button>
-        <button 
-          className={activeFilter === 'Ended' ? 'tok-active' : ''}
-          onClick={() => setActiveFilter('Ended')}
-        >
-          Ended
-        </button>
-        <button 
-          className={activeFilter === 'All' ? 'tok-active' : ''}
-          onClick={() => setActiveFilter('All')}
-        >
-          All
-        </button>
-        </div>
-        <div className="tokenclaims-wrapp">
-          {filteredTokens.map(((token, index) =>(  
-            <div className="tokenclaims-content glass-card" key={index}>
-              <Image src={token.img} alt={"nf"} width={500} height={300} className='token-img'/>
-              <div className="tokenclaims-content-text">
-                <div className="token-text-top">
-                  <p className={`current_condition ${activeFilter== "Ongoing"? "Ongoing":""} ${activeFilter== "Upcoming"? "Upcoming":""} ${activeFilter== "Ended"? "Ended":""} ${activeFilter== "All"? "All":""} `}>{token.condition}</p>
-                  <p className='date'>
-                    {token.startDate} - {token.endDate}
-                  </p>
-                </div>
-                <h2>{token.name}</h2>
-                <p>{token.desc}</p>
-                <button>{token.action}</button>
-              </div>
-            </div>
-          )))}
-        </div>
-      </div>
-
+      <TokenClaim />
         
     </div>
   )
